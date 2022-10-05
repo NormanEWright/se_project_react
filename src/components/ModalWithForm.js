850f002083637ce047249e0bba2a4c0374c0049c
= <n.wright.online@gmail.com>
2022-10-05 00:17:12 -0500

    Add condition for opening modal, fix noValidate

--- src/components/ModalWithForm.js
+++ src/components/ModalWithForm.js
@@ -4,8 +4,8 @@ import closeIcon from "../images/ModalWithForm/close-button.svg";
 
 function ModalWithForm(props) {
   return(
-    <div className={`popup popup_type_${props.name}`}>
-      <form action="submit" class="popup__form" id="add-garment-form" novalidate>
+    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
+      <form action="submit" className="popup__form" id="add-garment-form" noValidate>
         <h3 className="popup__form-heading">{props.title}</h3>
         {props.children}
         <button type="submit" className="popup__form-add-button" >{props.buttonText}</button>
