269b2fc5948e8f385186a7800800cbd51b8087fb
= <n.wright.online@gmail.com>
2022-10-05 00:16:21 -0500

    Add condition for opening modal

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -5,7 +5,7 @@ import "../blocks/item-modal.css";
 
 function ItemModal(props) {
   return(
-    <div className={`popup popup_type_${props.name}`}>
+    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__item-wrapper">
         <img className="popup__item-image" src={props.link} alt={props.title} />
         <p className="popup__item-title" >{props.title}</p>
