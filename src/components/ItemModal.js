00ba18858210a93ac99003a661bac83b202a7613
= <n.wright.online@gmail.com>
2022-10-06 21:51:57 -0500

    Modify component properties

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -3,14 +3,14 @@ import "../blocks/popup.css";
 import closeIcon from "../images/ItemModal/close-button.svg";
 import "../blocks/item-modal.css";
 
-function ItemModal(props) {
+function ItemModal({ name, isOpen, onClose, data }) {
   return(
-    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
+    <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__item-wrapper">
-        <img className="popup__item-image" src={props.link} alt={props.title} />
-        <p className="popup__item-title" >{props.title}</p>
-        <p className="popup__item-description" >Weather: {props.description}</p>
-        <button type="button" className="popup__close-button" onClick={props.onClose}>
+        <img className="popup__item-image" src={data.link} alt={data.title} />
+        <p className="popup__item-title" >{data.title}</p>
+        <p className="popup__item-description" >Weather: {data.description}</p>
+        <button type="button" className="popup__close-button" onClick={onClose}>
           <img className="popup__close-icon" src={closeIcon} alt="Close popup button" />
         </button>
       </div>
