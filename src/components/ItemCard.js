b3d48d444899743108aee16ffb4d8eb014c06957
= <n.wright.online@gmail.com>
2022-10-06 21:51:16 -0500

    Modify component to return single item card

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import '../blocks/card.css';
 
-function ItemCard(props) {
-  return (
-    <>
-      <ul className='card__list'>
-        {props.clothingList.map((card) => {
-          if (card.weather === props.currentWeather) {
-            return (
-              <li
-                key={card._id}
-                className="card"
-                style={{ backgroundImage: `url(${card.link})` }}
-                onClick={() => {
-                  props.openPopup("item", {
-                    link: card.link,
-                    title: card.name,
-                    description: card.weather,
-                  })
-                }}
-              >
-                <div>
-                  <h3 className='card__title'>{card.name}</h3>
-                </div>
-              </li>
-            );
-          }
-          return '';
-        })}
-      </ul>
-    </>
-  )
+function ItemCard({ data, openPopup, currentWeather }) {
+  const openPreview = () => {
+    openPopup("item", {
+      link: data.link,
+      title: data.name,
+      description: data.weather,
+    });
+  }
+  console.log(`Current weather is: ${currentWeather}`);
+  if (data.weather === currentWeather) {
+    return (
+      <li
+        key={data._id}
+        className="card"
+        style={{ backgroundImage: `url(${data.link})` }}
+        onClick={openPreview}
+      >
+        <div>
+          <h3 className='card__title'>{data.name}</h3>
+        </div>
+      </li >
+    )
+  }
 }
 
 export default ItemCard;
